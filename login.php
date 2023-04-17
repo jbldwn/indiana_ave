a6fafc2dcaaae1e5faf0a33d306124b42d5f3046
jbldwn <58793978+jbldwn@users.noreply.github.com>
2023-04-17 16:26:22 -0400

    Login restyled to match about section. now responsive.

--- login.php
+++ login.php
@@ -16,27 +16,42 @@ require_once('includes/headerBLK.php');
 
 ?>
 
+<!--tester content-->
+
 <!--login page content-->
 <section class="login">
     <div class="login-img"></div>
-
     <div class="container login-base">
-        <div class="pageHeader">
-            <h1>Login</h1>
+        <div class="login-form">
+            <h1 class="header">Login</h1>
+            <form method="post" action="verifyuser.php">
+                <?php
+                //            if ($login_status == 2) {
+                //                echo "<p style='margin: 0 auto 30px auto; color:#EE7B30;' class='p-textLarge'>Login failed. Please try again.</p>";
+                //            }
+                ?>
+                <label for="username">Username</label>
+                <input type="text" name="username">
+                <label for="password">Password</label>
+                <input type="password" name="password">
+                <input type="submit" value="Login" id="submit-button">
+                <!--            <a href="">Create Account</a>-->
+            </form>
         </div>
-        <form method="post" action="verifyuser.php">
-            <?php
-//            if ($login_status == 2) {
-//                echo "<p style='margin: 0 auto 30px auto; color:#EE7B30;' class='p-textLarge'>Login failed. Please try again.</p>";
-//            }
-            ?>
-            <label for="username">Username</label>
-            <input type="text" name="username">
-            <label for="password">Password</label>
-            <input type="password" name="password">
-            <input type="submit" value="Login" id="submit-button">
-<!--            <a href="">Create Account</a>-->
-        </form>
+<!--        <h1 class="header">Login</h1>-->
+<!--        <form method="post" action="verifyuser.php">-->
+<!--            --><?php
+//            //            if ($login_status == 2) {
+//            //                echo "<p style='margin: 0 auto 30px auto; color:#EE7B30;' class='p-textLarge'>Login failed. Please try again.</p>";
+//            //            }
+//            ?>
+<!--            <label for="username">Username</label>-->
+<!--            <input type="text" name="username">-->
+<!--            <label for="password">Password</label>-->
+<!--            <input type="password" name="password">-->
+<!--            <input type="submit" value="Login" id="submit-button">-->
+<!--                        <a href="">Create Account</a>-->
+<!--        </form>-->
     </div>
 </section>
 
