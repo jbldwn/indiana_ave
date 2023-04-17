9a1a56edbf83cfdb5c92350397d0c4c51c56d918
jbldwn <58793978+jbldwn@users.noreply.github.com>
2023-04-17 16:24:40 -0400

    Mobile white broke nav responsiveness broke. Repaired and functional now.

--- includes/header.php
+++ includes/header.php
@@ -37,80 +37,67 @@ if (isset($_SESSION['login'])
 <div class="container">
 
     <div class="hero">
-        <div class="nav-mobile-wrapper">
-            <div id="ham-icon" onclick="toggleMenu(this)">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <ul class="mobile-menu">
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="landmarks.php">Landmarks</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="login.php">Login</a></li>
-                </ul>
-            </div>
-            <div class="mobile-icon"></div>
-        </div>
-        <div class="nav-wrapper">
-            <nav>
-                <div class="links">
-                    <a href="index.php">Home</a>
-                    <a href="index.php#about">About</a>
-                    <a href="landmarks.php">Landmarks</a>
-                    <a href="contact.php">Contact</a>
+        <div class="nav-white">
+            <div class="mobile">
+                <div id="ham-icon" onclick="toggleMenu(this)">
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <ul class="mobile-menu">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="landmarks.php">Landmarks</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
                 </div>
-                <div class="logo"></div>
-            </nav>
-            <div class="user-panel">
-
-                <?php
+                <div class="mobile-icon"></div>
+            </div>
+            <div class="web">
+                <nav>
+                    <div class="links">
+                        <a href="index.php">Home</a>
+                        <a href="index.php#about">About</a>
+                        <a href="landmarks.php">Landmarks</a>
+                        <a href="contact.php">Contact</a>
+                    </div>
+                    <div class="logo"></div>
+                </nav>
+                <div class="user-panel">
+                    <?php
                     if($login!=null) {
                         echo "<span class='greeting'>Hello $name</span>
-                        <div class='ctrls'>";
+                <div class='ctrls'>";
                         if ($role == 2) {
                             echo "
-                            <button class='save'>
-                                <i class='fa-solid fa-clipboard-user fa-xl'></i>
-                                <span class='label'>Roster</span>
-                            </button>
-                            ";
+                    <button class='save'>
+                        <i class='fa-solid fa-clipboard-user fa-xl'></i>
+                        <span class='label'>Roster</span>
+                    </button>
+                    ";
                         }
                         if ($role == 0 || $role == 1 || $role == 2) {
                             //            echo "<span>basic user detected</span>";
                             //        }else if($role==1){
                             //            echo "<span>advanced user detected</span>";
                             echo "
-                                <button class='edit'>
-                                    <i class='fa-solid fa-user fa-xl'></i>
-                                    <span class='label'>Profile</span>
-                                </button>
-                                <a href='logout.php'>
-                                    <button class='delete'>
-                                        <i class='fa-solid fa-right-from-bracket fa-xl'></i>
-                                        <span class='label'>Logout</span>
-                                    </button>
-                                </a>
-                                </div>
-                            ";
+            <button class='edit'>
+                <i class='fa-solid fa-user fa-xl'></i>
+                <span class='label'>Profile</span>
+            </button>
+            <a href='logout.php'>
+                <button class='delete'>
+                    <i class='fa-solid fa-right-from-bracket fa-xl'></i>
+                    <span class='label'>Logout</span>
+                </button>
+            </a>
+            </div>
+            ";
                         }
                     }
 
 
-
-
-                ?>
-                <!--            <button class="delete">-->
-                <!--                <i class="fa-solid fa-right-from-bracket fa-xl"></i>-->
-                <!--                <span class="label">Logout</span>-->
-                <!--            </button>-->
-                <!--            <button class="edit">-->
-                <!--                <i class="fa-solid fa-user fa-xl"></i>-->
-                <!--                <span class="label">Profile</span>-->
-                <!--            </button>-->
-                <!--            <button class="save">-->
-                <!--                <i class="fa-solid fa-clipboard-user fa-xl"></i>-->
-                <!--                <span class="label">Roster</span>-->
-                <!--            </button>-->
+                    ?>
+                </div>
             </div>
         </div>
         <div class="pageHeader">
