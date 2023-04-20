c581a83997366909f6a3d8235bcff25c4c6a982f
jbldwn <58793978+jbldwn@users.noreply.github.com>
2023-04-20 09:57:21 -0400

    Profile control panel moved.

--- includes/header.php
+++ includes/header.php
@@ -75,19 +75,84 @@ if (isset($_SESSION['login'])
                     </button>
                     ";
                         }
+
+                        if ($role == 0 || $role == 1 || $role == 2) {
+                            //            echo "<span>basic user detected</span>";
+                            //        }else if($role==1){
+                            //            echo "<span>advanced user detected</span>";
+                            echo "
+            <a href='profile.php'>
+                <button class='edit'>
+                    <i class='fa-solid fa-user fa-xl'></i>
+                    <span class='label'>Profile</span>
+                </button>
+            </a> 
+            <a href='logout.php'>
+                <button class='delete'>
+                    <i class='fa-solid fa-right-from-bracket fa-xl'></i>
+                    <span class='label'>Logout</span>
+                </button>
+            </a>
+            </div>
+            ";
+                        }
+                    }
+
+
+                    ?>
+                </div>
+            </div>
+        </div>
+<!--        <div class="nav-mobile-wrapper">-->
+<!--            <div id="ham-icon" onclick="toggleMenu(this)">-->
+<!--                <div class="bar"></div>-->
+<!--                <div class="bar"></div>-->
+<!--                <div class="bar"></div>-->
+<!--                <ul class="mobile-menu">-->
+<!--                    <li><a href="index.php">Home</a></li>-->
+<!--                    <li><a href="landmarks.php">Landmarks</a></li>-->
+<!--                    <li><a href="contact.php">Contact</a></li>-->
+<!--                    <li><a href="login.php">Login</a></li>-->
+<!--                </ul>-->
+<!--            </div>-->
+<!--            <div class="mobile-icon"></div>-->
+<!--        </div>-->
+<!--        <div class="nav-white">-->
+<!--            <nav>-->
+<!--                <div class="links">-->
+<!--                    <a href="index.php">Home</a>-->
+<!--                    <a href="index.php#about">About</a>-->
+<!--                    <a href="landmarks.php">Landmarks</a>-->
+<!--                    <a href="contact.php">Contact</a>-->
+<!--                </div>-->
+<!--                <div class="logo"></div>-->
+<!--            </nav>-->
+<!--            <div class="user-panel">-->
+<!---->
+<!--                --><?php
+//                    if($login!=null) {
+//                        echo "<span class='greeting'>Hello $name</span>
+//                        <div class='ctrls'>";
+//                        if ($role == 2) {
+//                            echo "
+//                            <button class='save'>
+//                                <i class='fa-solid fa-clipboard-user fa-xl'></i>
+//                                <span class='label'>Roster</span>
+//                            </button>
+//                            ";
+//                        }
 //                        if ($role == 0 || $role == 1 || $role == 2) {
 //                            //            echo "<span>basic user detected</span>";
 //                            //        }else if($role==1){
 //                            //            echo "<span>advanced user detected</span>";
-//                            if(str_contains($page_title, 'Profile')){} else {echo "
+//                            echo "
 //                                <a href='profile.php'>
 //                                    <button class='edit'>
 //                                        <i class='fa-solid fa-user fa-xl'></i>
 //                                        <span class='label'>Profile</span>
 //                                    </button>
 //                                </a>
-//                            ";}
-//                            echo "<a href='logout.php'>
+//                                <a href='logout.php'>
 //                                    <button class='delete'>
 //                                        <i class='fa-solid fa-right-from-bracket fa-xl'></i>
 //                                        <span class='label'>Logout</span>
@@ -96,13 +161,17 @@ if (isset($_SESSION['login'])
 //                                </div>
 //                            ";
 //                        }
-                    }
-
-
-                    ?>
-                </div>
-            </div>
-        </div>
+//                    }
+//
+//
+//
+//
+//                ?>
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="pageHeader">-->
+<!--            <h1>Indiana Avenue</h1>-->
+<!--        </div>-->
         <div class="pageHeader">
             <h1>Indiana Avenue</h1>
         </div>
