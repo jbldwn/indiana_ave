abc0a05326038a99a8819149dc81fcfa3749bd89
jbldwn <58793978+jbldwn@users.noreply.github.com>
2023-04-25 22:10:34 -0400

    Location page control panel hidden based on login status.

--- landmarks.php
+++ landmarks.php
@@ -26,37 +26,48 @@ if (!$query) {
     die();
 }
 
+//check for admin
+$login = '';
+if(isset($_SESSION['login'])){
+    $login = $_SESSION['login'];
+}
+
 ?>
 <section class="landmarks container">
     <h1 id="ld-head">Landmarks</h1>
     <div class="all-landmarks">
         <div class="header">
             <h1 class="ld-body-head">All Locations</h1>
-            <div class="ctrls">
-                <button class="save">
-                    <div class="button-icon">
-                        <i class="fa-solid fa-plus fa-l"></i>
+            <?php
+            if($login!=null){
+                echo"
+                    <div class='ctrls'>
+                        <button class='save'>
+                            <div class='button-icon'>
+                                <i class='fa-solid fa-plus fa-l'></i>
+                            </div>
+                            <span>Add Location</span>
+                        </button>
                     </div>
-                    <span>Add Location</span>
-                </button>
-            </div>
+                ";
+            }?>
         </div>
-<!--        list to hold all retrieved locations-->
+        <!--        list to hold all retrieved locations-->
         <div class="list">
-<!--            creates a new link for each landmark from db-->
+            <!--            creates a new link for each landmark from db-->
             <?php while($row = $query->fetch_assoc()) { ?>
-            <a style="text-decoration: none; color: black"  href="landmark-detail.php?location_id=<?= $row['location_id'] ?>">
-                <div id="location_<?= $row['location_id']?>" class="location" style="background-image:linear-gradient(rgba(83, 73, 129, 0.5),rgba(83, 73, 129, 0.5)), url('www/assets/images/street.jpg')">
+                <a style="text-decoration: none; color: black"  href="landmark-detail.php?location_id=<?= $row['location_id'] ?>">
+                    <div id="location_<?= $row['location_id']?>" class="location" style="background-image:linear-gradient(rgba(83, 73, 129, 0.5),rgba(83, 73, 129, 0.5)), url('www/assets/images/street.jpg')">
 
-                    <!--site-img is just holding space to showcase img applied to parent-->
-                    <div class="site-img" ></div>
-                    <div class="site-info">
-                        <h1 class="loc_name"><?= $row['location_name'] ?></h1>
-                        <p class="address"><?= $row['location_address'] ?></p>
+                        <!--site-img is just holding space to showcase img applied to parent-->
+                        <div class="site-img" ></div>
+                        <div class="site-info">
+                            <h1 class="loc_name"><?= $row['location_name'] ?></h1>
+                            <p class="address"><?= $row['location_address'] ?></p>
+                        </div>
                     </div>
-                </div>
-            </a>
-        <?php } ?>
+                </a>
+            <?php } ?>
         </div>
 
     </div>
